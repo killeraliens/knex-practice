b79c41c709511ab2f847a50537c0b85133e22722
killeraliens <killeraliens@outlook.com>
2019-11-22 18:50:19 -0700

    using all the crud

--- src/blogful.js
+++ src/blogful.js
@@ -7,6 +7,31 @@ const knexInstance = knex({
 
 const ArticlesService = require('./articles-service');
 
+ArticlesService.getAllArticles(knexInstance)
+  .then(allArticles => {
+    console.log('ALL THE ARTICLES',allArticles)
+  })
+  .then(() => {
+    const newArticle = {
+      title: 'A story about Bronzi',
+      content: 'A day in the life.'
+    }
+    return ArticlesService.insertArticle(knexInstance, newArticle)
+  })
+  .then(newArticle => {
+    console.log('NEW ARTICLE', newArticle)
+    const updateContent = { title: 'A Story about RU'}
+    return ArticlesService.updateArticle(knexInstance, newArticle.id, updateContent)
+      .then(() => ArticlesService.getById(knexInstance, newArticle.id))
+  })
+  .then(article => {
+    console.log('UPDATED ARTICLE', article)
+    return ArticlesService.deleteArticle(knexInstance, article.id)
+  })
+
+
+
+
 
 //console.log(ArticlesService.quickQuery(knexInstance));
 //ArticlesService.quickQuery(knexInstance);
